9fba64c84a2b0d1f7a0135f4f3a481bcbfed18ab
Коновалов Александр Павлович <APKonovalov@sberbank.ru>
2021-02-26 13:25:10 +0300

    initial commit

---
+++ cypress/integration/book_training.js
@@ -0,0 +1,34 @@
+describe('Workout booking app', () => {
+  it('Book workout on this and next week', () => {
+    cy.visit(Cypress.env('login_url'))
+    login(Cypress.env('email'),Cypress.env('password'))
+    selectWorkout(Cypress.env('workout'))
+	book()
+	goToNextWeek()
+    book()
+  })
+})
+
+function login(email,password){
+	cy.get('[name="Login"]')
+      .type(email)
+      .should('have.value', email)
+    cy.get('[name="Password"]')
+      .type(password)
+      .should('have.value', password)
+    cy.get('#confirm').click()
+}
+
+function selectWorkout(workoutName){
+    cy.contains('Все программы').click()
+    cy.get('[name="search"]').type(workoutName + '{enter}')
+}
+
+function book(){
+	cy.wait(1000)
+    cy.contains('Участвовать').click({force: true})
+}
+
+function goToNextWeek(){
+	cy.get('.glyphicon-cp-button-right-1').click()
+}
